ef4b45fa2c6f8fb084c7194f65ab999fdfc31492
RiadElamine <RiadElamineDevInfo@gmail.com>
2024-11-28 22:19:30 +0100

    cpp03

--- ex03/Character.cpp
+++ ex03/Character.cpp
@@ -6,7 +6,7 @@
 /*   By: relamine <relamine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 17:00:07 by relamine          #+#    #+#             */
-/*   Updated: 2024/11/28 20:13:23 by relamine         ###   ########.fr       */
+/*   Updated: 2024/11/28 22:17:26 by relamine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,16 +22,28 @@ Character::Character(std::string Name)
 
 Character::~Character()
 {
+	clean_up();
+}
+Character::Character(Character &c)
+{
+	*this = c;
+}
+Character& Character::operator=(const Character& c)
+{
+	clean_up();
+	this->Name = c.getName();
 	for (int i = 0; i < 4; i++)
 	{
-		if (this->slots[i])
-		{
-			delete (this->slots[i]);
-			this->slots[i] = NULL;
-		}
+        if (c.slots[i])
+        {
+            this->slots[i] = c.slots[i]->clone();
+        }
+        else
+        {
+            this->slots[i] = NULL; 
+        }
 	}
 }
-
 
 std::string const & Character::getName() const
 {
@@ -65,4 +77,17 @@ void Character::use(int idx, Character& target)
 	if (slots[idx] == NULL)
 		return ((void)0);
 	slots[idx]->use(target);
+}
+
+
+void Character::clean_up()
+{
+	for (int i = 0; i < 4; i++)
+	{
+		if (this->slots[i])
+		{
+			delete (this->slots[i]);
+			this->slots[i] = NULL;
+		}
+	}
 }
--- ex03/Character.hpp
+++ ex03/Character.hpp
@@ -6,7 +6,7 @@
 /*   By: relamine <relamine@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/28 17:00:27 by relamine          #+#    #+#             */
-/*   Updated: 2024/11/28 17:24:37 by relamine         ###   ########.fr       */
+/*   Updated: 2024/11/28 22:19:14 by relamine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,17 @@
 class Character : public ICharacter
 {
 	private:
-		AMateria *slots[4];
 		std::string Name;
 	public:
+		AMateria *slots[4];
 		Character(std::string Name);
+		Character(Character &c);
+		Character& operator=(const Character& c);
 		~Character() {}
 		std::string const & getName() const;
 		void equip(AMateria* m);
 		void unequip(int idx);
 		void use(int idx, Character& target);
+		void clean_up();
 };
+
